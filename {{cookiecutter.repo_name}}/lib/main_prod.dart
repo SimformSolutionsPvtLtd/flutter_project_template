4c9bb5ca04f1bbbfcfb65618e9c761580d546c5b
vatsaltanna-simformsolutions <vatsal.t@simformsolutions.com>
2021-03-21 20:00:01 +0530

    :sparkles: flutter template using cookie cutter (in progress)

---
+++ {{cookiecutter.repo_name}}/lib/main_prod.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
+
+import 'app_config.dart';
+import 'locator.dart';
+import 'utils/helpers.dart';
+import 'flavors/flavor.dart';
+import 'flavors/flavor_config.dart';
+import 'flavors/flavor_values.dart';
+
+void main() {
+  setAppOrientation();
+  setupLogging();
+  setupLocator();
+  SentryFlutter.init(
+        (options) => options.dsn = 'https://{{cookiecutter.sentry_dsn_key}}@o0.ingest.sentry.io/0',
+    appRunner: () => runApp(FlavoredApp()),
+  );
+}
+
+class FlavoredApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return FlavorConfig(
+      flavor: Flavor.prod,
+      values: FlavorValues(baseUrl: 'https://api.revolve.net'),
+      child: AppConfig(),
+    );
+  }
+
+}
+
