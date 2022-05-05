a9f2e712d15f3d25c831efea799d4175aff20f36
DhvanitVaghani <vaghanidhvanit@gmail.com>
2022-05-05 19:45:47 +0530

    :sparkles: Add api logger for request, response and error, Remove .idea folder, Add .gitignore file in project structure

--- {{cookiecutter.repo_name}}/lib/apibase/header_intercepter.dart
+++ {{cookiecutter.repo_name}}/lib/apibase/header_intercepter.dart
@@ -4,9 +4,11 @@ import 'package:dio/dio.dart';
 
 import '../utils/network_utils.dart';
 import '../utils/shared_preferences_helper.dart';
+import 'api_logger.dart';
 
 class HeaderInterceptor extends Interceptor {
   final bool showLogs;
+  final APILogger _logger = APILogger();
 
   HeaderInterceptor({this.showLogs = true});
 
@@ -19,6 +21,13 @@ class HeaderInterceptor extends Interceptor {
       if (token.isNotEmpty) {
         options.headers.putIfAbsent('Authorization', () => '$token');
       }
+      _logger.printSuccessLog(
+        apiMethod: options.method,
+        responseBody: options.data.toString(),
+        parameters: options.queryParameters,
+        url: "${options.baseUrl}${options.path}",
+        token: options.headers["Authentication"] ?? "",
+      );
     } else {
       ///TODO:- Show no internet dialog or toast here.
     }
@@ -27,17 +36,40 @@ class HeaderInterceptor extends Interceptor {
 
   @override
   void onResponse(Response response,
-      ResponseInterceptorHandler responseInterceptorHandler) async {
+      ResponseInterceptorHandler responseInterceptorHandler) {
     if (response.statusCode == 401) {
       ///TODO:- Handle token expired
     }
+    _logger.printSuccessLog(
+      apiMethod: response.requestOptions.method,
+      responseBody: response.data.toString(),
+      parameters: response.requestOptions.data,
+      url: "${response.realUri.scheme}://${response.realUri.authority}"
+          "${response.realUri.path}",
+      token: response.headers.value("Authentication") ?? "",
+      isRequest: false,
+    );
     responseInterceptorHandler.resolve(response);
   }
 
   @override
   void onError(
-          DioError dioError, ErrorInterceptorHandler errorInterceptorHandler) =>
-      errorInterceptorHandler.reject(dioError);
+      DioError dioError, ErrorInterceptorHandler errorInterceptorHandler) {
+    if (dioError.response != null) {
+      _logger.printErrorLog(
+        responseBody: dioError.response!.data.toString(),
+        parameters: dioError.response!.requestOptions.data,
+        url:
+            "${dioError.response!.realUri.scheme}://${dioError.response!.realUri.authority}"
+            "${dioError.response!.realUri.path}",
+        token: dioError.response!.headers.value("Authentication") ?? "",
+        errorString: dioError.response!.statusMessage ?? "No message found",
+        statusCode: dioError.response!.statusCode ?? -1,
+      );
+    }
+
+    errorInterceptorHandler.reject(dioError);
+  }
 
   Future<String> checkToken() async =>
       await SharedPreferencesHelper.instance.getAuthToken();
